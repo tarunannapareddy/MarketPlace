514f7e1a9a139b4d5b9583bbd70c59ac60116736
rajendransurya <surya.rajendran@colorado.edu>
2024-01-25 00:16:50 -0700

    DB working intial code

--- src/pojos/Buyer.java
+++ src/pojos/Buyer.java
@@ -5,4 +5,14 @@ public class Buyer {
     String name;
     Integer itemsPurchased;
 
+    public void setBuyerId(Integer buyerId){this.buyerId=buyerId;}
+
+    public void setName(String name){this.name=name;}
+
+    public String getName(){return name;}
+
+    public void setItemsPurchased(Integer itemsPurchased){this.itemsPurchased=itemsPurchased;}
+
+    public Integer getItemsPurchased(){return itemsPurchased;}
+
 }
